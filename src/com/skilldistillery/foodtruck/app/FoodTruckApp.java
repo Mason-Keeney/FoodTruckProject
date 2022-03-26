f2138167e8d4beac2a05dadf2bd28cb6d045c529
Mason Keeney <mason.keeney68@gmail.com>
2022-03-25 18:58:30 -0600

    I accidentally wrote all of it in one sitting

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,137 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
+	private boolean isInputting = true;
+	private boolean menuShowing = true;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		TODO USER ENTERS NAME, FOODTYPE, AND RATING UP TO FIVE FOOD TRUCKS
+		
+//	USER INPUT LOOP VARIABLES
+		Scanner sc = new Scanner(System.in);
+		FoodTruck[] trucks = new FoodTruck[5];
+		FoodTruckApp app = new FoodTruckApp();
+		
+		
+		
+//	USER INPUT FOR LOOP BEGIN
+		for (int i = 0; i < 5; i++) {
+			if(app.isInputting == true) {
+			trucks[i] = app.rateNewTruck(sc, app);
+			} else {
+				break;
+			}
+		}
+		
+//	MENU LOOP VARIABLES
+		
+		while(app.menuShowing) {
+			app.printMenu();
+			int input = sc.nextInt();
+			app.menuSwitch(input, trucks, app);
+		}
+			
+		if(app.menuShowing) {
+			sc.close();
+		}
+		
+		
+		
+		
+//		TODO USER HAS QUIT OPTION
+		
+//		TODO MENU (ANYTIME QUIT OPTION??)
 
 	}
 
+	private FoodTruck rateNewTruck(Scanner sc, FoodTruckApp app) {
+		
+		System.out.print("Please enter the name of the Food Truck you would like to rate (enter quit to exit): ");
+		FoodTruck temp = new FoodTruck();
+		String name = sc.nextLine();
+			if (name.toLowerCase().equals("quit")) {
+				isInputting = false;
+				return null;
+			}
+		temp.setName(name);
+		System.out.print("Please enter the type of food that " + name +  " serves: ");
+		String food = sc.nextLine();
+		temp.setFoodType(food);
+		System.out.println("Now, how would you rate them?");
+		int rating = sc.nextInt();
+		sc.nextLine();
+		temp.setRating(rating);
+		
+		
+		return temp;
+		
+	}
+	
+	private void printMenu() {
+		System.out.println("---------------------------------------------");
+		System.out.println("|                                           |");
+		System.out.println("|          Please Select a number           |");
+		System.out.println("|                                           |");
+		System.out.println("| 1. List all Existing food trucks          |");
+		System.out.println("| 2. See the average rating of food trucks  |");
+		System.out.println("| 3. Display the highest-rated food truck   |");
+		System.out.println("| 4. Quit the program                       |");
+		System.out.println("|                                           |");
+		System.out.println("---------------------------------------------");
+	}
+	private void menuSwitch(int choice, FoodTruck[] trucks, FoodTruckApp app) {
+		switch (choice) {
+		case 1:
+			for (FoodTruck truck : trucks) {
+				if (truck != null) {
+				System.out.println(truck.toString());
+				}
+			}
+			break;
+		case 2:
+			System.out.println(app.averageRating(trucks));
+			break;
+		case 3:
+			FoodTruck best = app.highestRating(trucks);
+			System.out.println("The best truck is " + best.getName() + "  at a rating of: " + best.getRating());
+			break;
+		case 4:
+			menuShowing = false;
+			return;
+			
+		}
+	}
+	private FoodTruck highestRating(FoodTruck[] trucks) {
+		FoodTruck best = trucks[0];
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				if (truck.getRating() > best.getRating()) {
+					best = truck;
+				}
+			}
+		}
+		return best;
+		
+	}
+
+	private int averageRating(FoodTruck[] trucks) {
+		int average = 0;
+		int numTruck = 0;
+		for (FoodTruck truck : trucks) {
+			if (truck != null) {
+				average += truck.getRating();
+				numTruck++;
+			} else {
+				break;
+			}
+		}
+		average /= numTruck;
+		
+		return average;
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -2,17 +2,77 @@ package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
 	private static int idShift = 1;
-	private final String ID = getID();
+	private final String Id = setId();
 	private String name;
-	private int Rating;
-	private String[] employees;
+	private String foodType;
+	private int rating;
+
 	
-	
-	
-	
-	private String getID() {
-		String id = "";
+	public FoodTruck() {
 		
-		return id;
 	}
+	
+	public FoodTruck(String name, String foodType, int rating) {
+		this.name = name;
+		this.rating = rating;
+		this.foodType = foodType;
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
+	private String setId() {
+		String newId = "";
+		StringBuilder sb = new StringBuilder();
+		
+//	RANDOMIZES A TEN PLACE NUMERIC ID
+		for (int i = 0; i < 12; i++) {
+			int random = (int)Math.round(Math.random() * 6);
+				sb.append(((random * (i + idShift)) % 10));	
+				if (i % 3 == 0) {
+					sb.append('-');
+				}
+		}
+		
+//	INCREMENT IDSHIFT	
+		idShift++;
+		
+//	BUILD NEWID WITH STRINGBUILDER
+		newId = sb.toString();
+		return newId;
+	}
+	
+	public String getId() {
+		return Id;
+	}
+	
+	@Override
+	public String toString() {
+		return "FoodTruck [ID: " + Id + " || Name: " + name + " || Serves: " + foodType + " || Rating: " + rating + "]";
+	}
+	
 }
